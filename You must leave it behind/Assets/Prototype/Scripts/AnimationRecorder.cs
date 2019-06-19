18c604e18059e5db4b559544aaf90c20de0877bf
Ben <Bennyman54@yahoo.co.uk>
2019-06-19 01:09:51 +0100

    Added Recording
    Everyone with an animator recorder that is added to the SwitchHanlder's array of objects now will repeat their actions.
    
    Needs a lot of smoothing out but currently works.

---
+++ You must leave it behind/Assets/Prototype/Scripts/AnimationRecorder.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRecorder : MonoBehaviour
+{
+
+    public bool ShouldRecord,shouldPlay;
+
+    public Vector3[] pos;
+
+    private int curPoint,speed;
+
+    public void StartRecording()
+    {
+        curPoint = 0;
+        pos = new Vector3[250];
+        StartCoroutine("Recording");
+    }
+
+    public void PlayRecording()
+    {
+        curPoint = 0;
+        if (pos.Length > 0)
+        {
+            transform.position = pos[0];
+        }
+        StartCoroutine("PlayingRecording");
+        StartCoroutine("ContinuePlaying");
+    }
+
+
+    IEnumerator Recording()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        if (ShouldRecord)
+        {
+            //Make sure to track curSpeed,rotation etc so we can get an accurate recording
+            pos[curPoint] = gameObject.transform.position;
+            curPoint++;
+            StartCoroutine("Recording");
+        }
+        else
+        {
+            //Add final Record here
+            pos[curPoint] = gameObject.transform.position;
+        }
+
+    }
+
+    IEnumerator PlayingRecording()
+    {
+        yield return new WaitForSeconds(.1f);
+
+        if (pos.Length > 0)
+        {
+            if (pos[curPoint] != new Vector3(0, 0, 0))
+            {
+                curPoint+=1;
+                StartCoroutine("PlayingRecording");
+            }
+            else
+            {
+                shouldPlay = false;
+            }
+        }
+
+
+
+    }
+
+    IEnumerator ContinuePlaying()
+    {
+        yield return new WaitForSeconds(.01f);
+
+        if (pos.Length > 0)
+        {
+            if (shouldPlay && pos[curPoint] != new Vector3(0,0,0))
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos[curPoint], 5 * Time.deltaTime);
+                StartCoroutine("ContinuePlaying");
+            }
+        }
+
+
+    }
+
+
+}
--- You must leave it behind/Assets/Prototype/Scripts/SpiritSwitch.cs
+++ You must leave it behind/Assets/Prototype/Scripts/SpiritSwitch.cs
@@ -6,13 +6,40 @@ public class SpiritSwitch : MonoBehaviour
 {
 
     public GameObject Player, Spirit,sCamera,pCamera;
+
+    public AnimationRecorder[] allRecorders;
+
     public SpiritMove moveSpirit;
+    private AnimationRecorder animRecord;
+
+    private GameObject curBody, curCamera;
 
     public Image dot,fade;
 
     public GameObject distSlider;
 
-    public float speed;
+    public float speed,timerMax,timerSpeed;
+
+    private float timer;
+
+    private bool curSpirit;
+
+
+
+    private void Start()
+    {
+        animRecord = gameObject.GetComponent<AnimationRecorder>();
+        curBody = Player;
+        curCamera = pCamera;
+    }
+
+
+
+
+
+
+
+
     public void SwitchToPlayerBody(GameObject bodyToSwitch)
     {
         if (bodyToSwitch == null)
@@ -30,8 +57,10 @@ public class SpiritSwitch : MonoBehaviour
         if (bodyToSwitch == Player)
         {
             moveSpirit.speed = 0;
-            Spirit.transform.position = new Vector3(999, 999, 999);
-            Spirit.SetActive(false);
+            //Spirit.transform.position = new Vector3(999, 999, 999);
+            //Spirit.SetActive(false);
+            sCamera.SetActive(false);
+            moveSpirit.enabled = false;
 
             PlayerMove playMov = Player.GetComponent<PlayerMove>();
             playMov.enabled = true;
@@ -41,17 +70,43 @@ public class SpiritSwitch : MonoBehaviour
             dot.color = new Color(dot.color.r, dot.color.g, dot.color.b, 0.3f);
 
             fade.color = new Color(fade.color.r, fade.color.g, fade.color.b, 1);
+
+            curSpirit = false;
+
+            curCamera = pCamera;
+            curBody = Player;
+
+            foreach (AnimationRecorder anim in allRecorders)
+            {
+                anim.ShouldRecord = false;
+                anim.shouldPlay = true;
+                anim.PlayRecording();
+            }
+
+            StopCoroutine("countTimer");
+
+            //animRecord.ShouldRecord = false;
+            //animRecord.shouldPlay = true;
+            //animRecord.PlayRecording();
+
             StartCoroutine("FadeIn");
 
         }
         else
         {
             moveSpirit.speed = 0;
-            Spirit.transform.position = new Vector3(999, 999, 999);
-            Spirit.SetActive(false);
+            sCamera.SetActive(false);
+            moveSpirit.enabled = false;
+            //Spirit.transform.position = new Vector3(999, 999, 999);
+            //Spirit.SetActive(false);
 
             EnemyAI ai = bodyToSwitch.GetComponent<EnemyAI>();
             ai.GetPossesed();
+
+            curSpirit = false;
+
+            curCamera = ai.controlledCamera;
+            curBody = bodyToSwitch;
 
             dot.color = Color.white;
             dot.color = new Color(dot.color.r, dot.color.g, dot.color.b, 0.3f);
@@ -69,10 +124,32 @@ public class SpiritSwitch : MonoBehaviour
         sCamera.transform.rotation = bodyCamera.transform.rotation;
         bodyCamera.SetActive(false);
         Spirit.SetActive(true);
+        moveSpirit.enabled = true;
+        sCamera.SetActive(true);
         fade.color = new Color(fade.color.r, fade.color.g, fade.color.b, 1);
         moveSpirit.vulnrable = true;
+        curSpirit = true;
+
+
+        PlayerMove aiP = body.GetComponent<PlayerMove>();
+        aiP.enabled = false;
+
 
         StartCoroutine("FadeIn");
+        
+        if (body == Player)
+        {
+            foreach (AnimationRecorder anim in allRecorders)
+            {
+                anim.ShouldRecord = true;
+                anim.shouldPlay = false;
+                anim.StartRecording();
+            }
+
+
+            timer = 0;
+            StartCoroutine("countTimer");
+        }
     }
 
     public void PossessEnemy()
@@ -111,6 +188,35 @@ public class SpiritSwitch : MonoBehaviour
             StartCoroutine("FadeIn");
         }
 
+    }
+
+    IEnumerator countTimer()
+    {
+        yield return new WaitForSeconds(.01f);
+
+        timer += timerSpeed * Time.deltaTime;
+
+        if (timer > timerMax)
+        {
+            if (!curSpirit)
+            {
+                SwitchToSpiritForm(curBody, curCamera);
+            }
+
+            StartCoroutine("SpiritGoToBody", Player);
+        }
+        else
+        {
+            StartCoroutine("countTimer");
+        }
+
+    }
+
+    IEnumerator delayedBackToPlayer()
+    {
+        yield return new WaitForSeconds(.2f);
+
+        StartCoroutine("SpiritGoToBody", Player);
     }
 
 }
